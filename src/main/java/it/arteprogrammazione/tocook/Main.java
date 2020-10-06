54216f57ff41028d8dfae26c2ef17446c2ef3c7b
gpisa00 <giuseppepisa00@gmail.com>
2020-10-06 20:35:46 +0200

    Prima commit from intellij

--- src/main/java/it/arteprogrammazione/tocook/Main.java
+++ src/main/java/it/arteprogrammazione/tocook/Main.java
@@ -2,12 +2,12 @@ package it.arteprogrammazione.tocook;
 
 public class Main {
 
-	public static void main (String[] args){
-		if(args.length == 0){
-			System.out.println("Hello World");			
-		}else{
-			System.out.println("Hello " + args[0]);	
-		}
-	}
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else {
+            System.out.println("Hello " + args[0]);
+        }
+    }
 
 }
